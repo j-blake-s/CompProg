de7703913f55ddd0d4164e782083340605e0ff19
Blake <blakeseekings25@gmail.com>
2021-09-19 01:12:56 -0400

    initial commit

---
+++ Problems/TrashBins/src/TrashBins.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "../headers/slink.h"
+
+using std::string;
+
+
+const std::string filepath = "../data/sample.txt";
+int main() {
+    if (!sopen(std::cin,filepath)) return 0;  // Don't Touch
+
+    int T,N;
+    string S;
+
+    std::cin >> T;
+
+    for (int t = 1; t<=T;t++) {
+        std::cin >> N;
+        std::cin >> S;
+
+
+
+        string x = "0";
+        std::cout << "Case #" << t << ": " << x << std::endl;
+    }
+
+    sclose(std::cin);  // Don't Touch
+    return 0;
+}
